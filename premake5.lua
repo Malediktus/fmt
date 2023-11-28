7c77d2cd6aeb2c68ff2ccb980b2f4df0305a43fd
Nico Grundei <malediktusrgb@gmail.com>
2023-11-28 22:32:19 +0100

    removed to bare minimum and switched to premake

---
+++ premake5.lua
@@ -0,0 +1,25 @@
+-- premake5.lua
+
+project "fmt"
+   kind "StaticLib"
+   language "C++"
+   cppdialect "C++20"
+   targetdir "bin/%{cfg.buildcfg}"
+   staticruntime "off"
+
+   files {
+      "include/fmt/*.h",
+      "src/format.cc",
+   }
+
+   includedirs {
+      "include"
+   }
+
+   filter "configurations:Debug"
+      defines { "DEBUG" }
+      symbols "On"
+
+   filter "configurations:Release"
+      defines { "NDEBUG" }
+      optimize "On"
